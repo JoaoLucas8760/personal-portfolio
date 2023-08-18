51cdfce3b6551788e3a74fc67587f55b0e6b401f
João Lucas Cardoso Correia <canalluka8760@gmail.com>
2023-08-18 13:30:03 -0300

    update projects

--- projects.ts
+++ projects.ts
@@ -9,45 +9,51 @@ import sunize from "./src/public/projects/sunize.png";
 const projects = [
   {
     id: "project-1",
-    title: "SquashCodes",
+    title: "Pizzaria Toffanetto",
     subtitle: "Web",
-    image: squashcodes,
-    href: "https://squashcodes.com/",
+    image:
+      "https://raw.githubusercontent.com/everton-dgn/pizzaria_toffanetto/master/screenshots/desktop.jpg",
+    href: "#",
   },
   {
     id: "project-2",
-    title: "Dashboard for minecraft server",
-    subtitle: "Web ( Deprecated )",
-    image: thelion,
-    href: "#",
+    title: "Dogs Book - Social network for your pets",
+    subtitle: "Web",
+    image:
+      "https://user-images.githubusercontent.com/72713975/210123571-60e135bc-cd40-4d2c-9241-29c95d1acaba.png",
+    href: "https://dogs-react-seven.vercel.app/",
   },
   {
     id: "project-3",
-    title: "Minecraft server website",
-    subtitle: "Web ( Deprecated )",
-    image: madland,
-    href: "#",
+    title: "MoviesLib -  The Movies Library",
+    subtitle: "Web ",
+    image:
+      "https://user-images.githubusercontent.com/72713975/212524770-489092d5-fb27-4d55-92b4-4867d3e609dc.png",
+    href: "https://movies-lib-peach.vercel.app/",
   },
   {
     id: "project-4",
-    title: "ButterFlyGames",
+    title: "Manual For Life - Web Course Platform",
     subtitle: "Web",
-    image: butterfly,
-    href: "https://www.butterflygames.net",
+    image:
+      "https://github-production-user-asset-6210df.s3.amazonaws.com/72713975/261661150-7b0edc5a-5f61-4f29-9c80-f02648c0b53d.png",
+    href: "https://manualfor.life/",
   },
   {
     id: "project-5",
-    title: "BGO - Brasil Gaming Online",
+    title: "Bella Olonje",
     subtitle: "Web",
-    image: bgo,
-    href: "https://bgogta.com.br/",
+    image:
+      "https://jldevdot.netlify.app/_ipx/w_384,q_75/https%3A%2F%2Fuser-images.githubusercontent.com%2F72713975%2F226064794-7774113b-d2d3-4964-8574-e214fc5cbea7.png?url=https%3A%2F%2Fuser-images.githubusercontent.com%2F72713975%2F226064794-7774113b-d2d3-4964-8574-e214fc5cbea7.png&w=384&q=75",
+    href: "https://tailwind-landing-page.netlify.app/",
   },
   {
     id: "project-6",
-    title: "BVL - Brasil Vida Leve",
+    title: "Taxi Up",
     subtitle: "Web",
-    image: bvl,
-    href: "https://bvlmta.com.br/",
+    image:
+      "https://jldevdot.netlify.app/_ipx/w_384,q_75/https%3A%2F%2Fraw.githubusercontent.com%2Fwaelhassine%2FDriverUP-Company%2Fmain%2Fpublic%2Fimage-taxi-app.png?url=https%3A%2F%2Fraw.githubusercontent.com%2Fwaelhassine%2FDriverUP-Company%2Fmain%2Fpublic%2Fimage-taxi-app.png&w=384&q=75",
+    href: "https://driver-up-company.vercel.app/",
   },
   {
     id: "project-7",
